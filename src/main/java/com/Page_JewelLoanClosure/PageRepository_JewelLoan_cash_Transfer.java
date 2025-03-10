754c3ca0c0dbfdb8feddf866e020d86462c6bae5
Vinushashetty <vinusha.shetty@speridian.com>
2025-03-10 11:10:03 +0530

    Fourth commit Jewel Loan Closure Cash and transfer

--- src/main/java/com/Page_JewelLoanClosure/PageRepository_JewelLoan_cash_Transfer.java
+++ src/main/java/com/Page_JewelLoanClosure/PageRepository_JewelLoan_cash_Transfer.java
@@ -4,8 +4,65 @@ import org.openqa.selenium.By;
 
 public class PageRepository_JewelLoan_cash_Transfer {
 
-	public By OpenClosureandrenewal = By.xpath("/html/body/form/div[7]/div[1]/div/div/div/div/div/div/div/ul/li[8]/div");
-	public By Clickonclosure= By.xpath("/html/body/form/div[7]/div[1]/div/div/div/div/div/div/div/ul/li[8]/ul/li[7]/a");
-	public By selectproduct=By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_ucTransactionAccInfo_ddlProductGroup\"]");
-	public By productname= By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_ucTransactionAccInfo_ddlProduct_ddl\"]");
+	public By OpenClosureandrenewal = By
+			.xpath("/html/body/form/div[7]/div[1]/div/div/div/div/div/div/div/ul/li[8]/div");
+	public By Clickonclosure = By
+			.xpath("/html/body/form/div[7]/div[1]/div/div/div/div/div/div/div/ul/li[8]/ul/li[7]/a");
+	public By selectproduct = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_ucTransactionAccInfo_ddlProductGroup\"]");
+	public By productname = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_ucTransactionAccInfo_ddlProduct_ddl\"]");
+	public By StoreAccun = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_ucTransactionAccInfo_txtAcNo_txt\"]");
+	public By clickGo = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_ucTransactionAccInfo_btnGo\"]");
+	public By ClickNo = By.xpath("/html/body/form/div[12]/div[3]/div/button[2]/span");
+	public By Totalamount = By
+			.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_ucTransactionRecoveryDue_txtTotalAllDue_txt\"]");
+	public By Selectcash = By
+			.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_lstApproval_ctrl0_ucClosureApproval_ddlTransactionMode_ddl\"]");
+	public By scrolldown = By.xpath("//*[@id=\"WorkArea\"]");
+	public By scrolldownside = By.xpath("//*[@id=\"CreateMenu\"]");
+
+	
+	public By remarks = By
+			.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_lstApproval_ctrl0_ucClosureApproval_txtRemarks_txt\"]");
+	public By submit = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_btnSubmit\"]");
+	public By TrasidVisible = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_lstSummary_ctrl1_lbllstTransID\"]");
+	public By Singout = By.xpath("/html/body/form/div[3]/div[2]/a[6]/strong");
+
+	// another user
+
+	public By authandcancel = By.xpath("/html/body/form/div[7]/div[1]/div/div/div/div/div/div/div/ul/li[10]/div");
+	public By Clickcashierauth = By
+			.xpath("/html/body/form/div[7]/div[1]/div/div/div/div/div/div/div/ul/li[10]/ul/li[8]/a");
+	public By clickrefresh = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_btnRefresh\"]");
+	public By checkbox = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_ucAB_lvwBatchDetails_ctrl0_chkSelect\"]");
+	public By transIdd = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_lstSummary_ctrl1_lbllstTransID\"]");
+	public By clickauths = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_btnSubmit\"]");
+	public By close = By.xpath("//*[@id=\"closetop\"]");
+	
+	//manager
+	
+	public By managertab = By.xpath("/html/body/form/div[7]/div[1]/div/div/div/div/div/div/div/ul/li[10]/ul/li[4]/a");
+	public By cashtab = By.xpath("//*[@id=\"__tab_ctl00_ctl00_CPH1_PRDCNT_tbMgr_tpCash\"]");
+	public By refreshtab = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_btnRefresh\"]");
+	public By checkboxman = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_tbMgr_tpCash_ucMACashier_lvwBatchDetails_ctrl0_chkSelect\"]");
+	public By clickauth = By.xpath("//input[@value='Authorize']");
+	public By close2 = By.xpath("//*[@id=\"closetop\"]");	
+	
+	//transfer
+	
+	public By selecttransfer= By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_lstApproval_ctrl0_ucClosureApproval_ddlTransactionMode_ddl\"]");
+public By cloamtdisplay= By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_lstTransApprovingInfo_ctrl0_ucLoanClosure_txtTrnAmount_txt\"]");
+public By clickPD = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_lstApproval_ctrl0_ucClosureApproval_btnAutoPost\"]");
+
+public By produgroup= By.xpath("//*[@id=\"ddlProductGroup_ddl\"]");
+public By produname= By.xpath("//*[@id=\"ddlProductName_ddl\"]");
+public By enteramount= By.xpath("//input[@title=\"Amount\"]");
+public By accnumber= By.xpath("//*[@id=\"txtChildAcNo_txt\"]");
+public By CliADD= By.xpath("//*[@id=\"btnAdd\"]");
+public By clickSubmit= By.xpath("//*[@id=\"btnSubmit\"]");
+
+public By submitpostD= By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_btnSubmit\"]");
+public By ttransid= By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_lstSummary_ctrl1_lbllstTransID\"]");
+public By Transfertab= By.xpath("//*[@id=\"__tab_ctl00_ctl00_CPH1_PRDCNT_tbMgr_tpTransfer\"]");
+public By dispname= By.xpath("//input[@id='txtTotalCrDr_txt']");
+public By checkboxtrue = By.xpath("//*[@id=\"ctl00_ctl00_CPH1_PRDCNT_tbMgr_tpTransfer_ucMATransfer_lvwBatchDetails_ctrl0_chkSelect\"]");
 }
--- src/test/java/com/test/JewelLoanClosure/AllScenario_JewelLoan_Cash_Transfer.java
+++ src/test/java/com/test/JewelLoanClosure/AllScenario_JewelLoan_Cash_Transfer.java
@@ -21,73 +21,105 @@ import com.listeners.TestListener;
 
 public class AllScenario_JewelLoan_Cash_Transfer {
 
-		com.Utility.ExcelReader ExcelReader;
-		Base_Class Base_Class;
-		Log log;
-		TestListener TestListener;
-		com.Utility.ScreenShot screenShot;
-		
-		PageRepositary_AccOpn_LoanOpn_JewelLoan_GoldLoan goaldLoanRepo = new PageRepositary_AccOpn_LoanOpn_JewelLoan_GoldLoan();
-		com.Page_Customer.Customer_CustomerSearch custSrchMthds = new com.Page_Customer.Customer_CustomerSearch();
-		Customer_NewCustomer newCustMths = new Customer_NewCustomer();
-		General_Opening_cash_transfer generalOpening = new General_Opening_cash_transfer();
-		JewelLoan_Closure_Cash_Transfer JLoanClosure = new JewelLoan_Closure_Cash_Transfer();
-		
-		@BeforeSuite
-		public void reference() {
-			ExcelReader = new com.Utility.ExcelReader("JewelClosure");
-			log = new Log();
-			TestListener = new TestListener();
-			screenShot = new com.Utility.ScreenShot(null);
-			Base_Class = new Base_Class();
-		}
+	com.Utility.ExcelReader ExcelReader;
+	Base_Class Base_Class;
+	Log log;
+	TestListener TestListener;
+	com.Utility.ScreenShot screenShot;
 
-		@Test(dataProvider = "TestData")
-		public void newCustomer(Map<Object, Object> testdata, ITestContext context)
-				throws ClassNotFoundException, InterruptedException, IOException {
-			try {
-				if (testdata.get("Run").toString().equalsIgnoreCase("Yes")) {
-					ExtentTestManager.startTest(testdata.get("TestScenario").toString());
-					Log.info("*** Running test method " + testdata.get("TestScenario").toString() + "...");
-					context.setAttribute("fileName", "Login");
+	public String sp = "GetJewelloanAccountnumber";
+	public String columnName = "Acno";
 
-					// TC No. - 01 --> Application launch
-					ExtentTestManager.startTest("Application launch");
-					Base_Class.SetUp();
-					ExtentTestManager.endTest();
-					Thread.sleep(2000);
+	PageRepositary_AccOpn_LoanOpn_JewelLoan_GoldLoan goaldLoanRepo = new PageRepositary_AccOpn_LoanOpn_JewelLoan_GoldLoan();
+	com.Page_Customer.Customer_CustomerSearch custSrchMthds = new com.Page_Customer.Customer_CustomerSearch();
+	Customer_NewCustomer newCustMths = new Customer_NewCustomer();
+	General_Opening_cash_transfer generalOpening = new General_Opening_cash_transfer();
+	JewelLoan_Closure_Cash_Transfer JLoanClosure = new JewelLoan_Closure_Cash_Transfer();
 
-					custSrchMthds.pcRegistration(testdata, context);
-
-					custSrchMthds.userLoginValidPaswrd(testdata, context);
-					
-					JLoanClosure.Closurerenewal();
-					
-					
-					ExtentTestManager.endTest();
-					ExtentManager.getInstance().flush();
-				}
-			} catch (Exception e) {
-				ExtentTestManager.getTest().log(Status.FAIL, e);
-				Log.info("Exception " + e);
-				ExtentTestManager.endTest();
-			}
-		}
-
-		@DataProvider(name = "TestData")
-		public static Object[][] gettestdate() throws IOException {
-
-			Object[][] objectarry = null;
-			java.util.List<Map<String, String>> completedata = com.Utility.ExcelReader.getdata();
-
-			objectarry = new Object[completedata.size()][1];
-
-			for (int i = 0; i < completedata.size(); i++) {
-				objectarry[i][0] = completedata.get(i);
-			}
-			return objectarry;
-		}
-
+	@BeforeSuite
+	public void reference() {
+		ExcelReader = new com.Utility.ExcelReader("JewelClosure");
+		log = new Log();
+		TestListener = new TestListener();
+		screenShot = new com.Utility.ScreenShot(null);
+		Base_Class = new Base_Class();
 	}
 
+	@Test(dataProvider = "TestData")
+	public void newCustomer(Map<Object, Object> testdata, ITestContext context)
+			throws ClassNotFoundException, InterruptedException, IOException {
+		try {
+			if (testdata.get("Run").toString().equalsIgnoreCase("Yes")) {
+				ExtentTestManager.startTest(testdata.get("TestScenario").toString());
+				Log.info("*** Running test method " + testdata.get("TestScenario").toString() + "...");
+				context.setAttribute("fileName", "Login");
 
+				// TC No. - 01 --> Application launch
+				ExtentTestManager.startTest("Application launch");
+				Base_Class.SetUp();
+				ExtentTestManager.endTest();
+				Thread.sleep(2000);
+
+				custSrchMthds.pcRegistration(testdata, context);
+
+				custSrchMthds.userLoginValidPaswrd(testdata, context);
+
+				JLoanClosure.Closurerenewal();
+
+				JLoanClosure.Accountnumber(testdata, context);
+				
+				JLoanClosure.Cash(testdata, context);
+
+				JLoanClosure.Signout();
+
+				JLoanClosure.AnotherUSer(testdata, context);
+
+				JLoanClosure.Cashier();
+
+				JLoanClosure.Manager();
+
+				JLoanClosure.Signout();
+
+				custSrchMthds.userLoginValidPaswrd(testdata, context);
+
+				JLoanClosure.ClosurerenewalTrasnfer();
+
+				JLoanClosure.Accountnumber(testdata, context);
+
+				JLoanClosure.Transfer(testdata, context);
+
+				JLoanClosure.Accountnumber2(testdata, context);
+
+				JLoanClosure.Signout();
+
+				JLoanClosure.AnotherUSer(testdata, context);
+
+				JLoanClosure.ManagerAuth();
+
+				JLoanClosure.Signout();
+
+				ExtentTestManager.endTest();
+				ExtentManager.getInstance().flush();
+			}
+		} catch (Exception e) {
+			ExtentTestManager.getTest().log(Status.FAIL, e);
+			Log.info("Exception " + e);
+			ExtentTestManager.endTest();
+		}
+	}
+
+	@DataProvider(name = "TestData")
+	public static Object[][] gettestdate() throws IOException {
+
+		Object[][] objectarry = null;
+		java.util.List<Map<String, String>> completedata = com.Utility.ExcelReader.getdata();
+
+		objectarry = new Object[completedata.size()][1];
+
+		for (int i = 0; i < completedata.size(); i++) {
+			objectarry[i][0] = completedata.get(i);
+		}
+		return objectarry;
+	}
+
+}
